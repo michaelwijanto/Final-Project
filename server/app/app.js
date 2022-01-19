f3ccabd62067e431de71bdb3bef7a21cc1ff0843
Whisnubudih <whisnubudih@gmail.com>
2022-01-19 22:52:35 +0700

    content ++

--- server/app/app.js
+++ server/app/app.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== "production") { require('dotenv').config(); }
 const express = require('express')
 const app = express()
 const cors = require("cors")
-const router = require("./routes/index")
+const router = require("./routes/contentRouter")
 
 app.use(cors())
 app.use(express.json());
