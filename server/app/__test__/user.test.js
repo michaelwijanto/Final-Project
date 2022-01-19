f3c1e77428c7715197e01be006a5cbe51ee2599c
michaelwijanto <michael.wijanto@gmail.com>
2022-01-20 01:10:20 +0700

    test successful registration

--- server/app/__test__/user.test.js
+++ server/app/__test__/user.test.js
@@ -25,6 +25,27 @@ afterAll(async () => {
   });
 });
 
-describe("POST /users/register", () => {
-  test("should successfully register with status code 201");
+describe("POST /api/users/register", () => {
+  test("should successfully register with status code 201", (done) => {
+    request(app)
+      .post("/api/users/register")
+      .send({
+        email: "new@email.com",
+        password: "password",
+        fullName: "test name",
+      })
+      .then((response) => {
+        const result = response.body;
+        expect(response.status).toBe(201);
+        expect(result).toEqual(expect.any(Object));
+        expect(result).toHaveProperty("id", expect.any(Number));
+        expect(result).toHaveProperty("email", userTest.email);
+        expect(result).toHaveProperty("role", "user");
+        expect(result).toHaveProperty("isRegister", "false");
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
 });
