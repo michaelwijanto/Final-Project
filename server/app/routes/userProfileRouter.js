985765cee56bb48a8218c20de001b30667b8aaec
greoandre <gregoriusandre46@gmail.com>
2022-01-20 22:59:15 +0700

    update

--- server/app/routes/userProfileRouter.js
+++ server/app/routes/userProfileRouter.js
@@ -1,6 +1,7 @@
 const userProfileRouter = require("express").Router();
 const userProfilesController = require("../controllers/userProfilesController");
 
+userProfileRouter.get("/", userProfilesController.getUserProfile);
 userProfileRouter.post("/", userProfilesController.createUserProfile);
 
 userProfileRouter.patch("/updateSubs", userProfilesController.updateSubscription);
