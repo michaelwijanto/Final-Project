98dbed3de49581cab57583a8e43a278240e15e48
greoandre <gregoriusandre46@gmail.com>
2022-01-22 22:36:38 +0700

    set Apollo

--- client/App.js
+++ client/App.js
@@ -9,38 +9,42 @@ import { NativeBaseProvider, Box } from "native-base";
 import Home from "./src/screens/Home";
 import Contents from "./src/screens/Content";
 import LevelContent from "./src/components/LevelContent";
+import client from "./config/apolloClient";
+import { ApolloProvider } from "@apollo/client";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          {/* <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{
-              headerShown: false,
-            }}
-          /> */}
-          <Stack.Screen
-            name="Contents"
-            component={Contents}
-            options={{
-              headerShown: true,
-            }}
-          />
-          {/* <Stack.Screen
-            name="Level"
-            component={LevelContent}
-            options={{
-              headerShown: true,
-            }}
-          /> */}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
+    <ApolloProvider client={client}>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Contents"
+              component={Contents}
+              options={{
+                headerShown: true,
+              }}
+            />
+            <Stack.Screen
+              name="Level"
+              component={LevelContent}
+              options={{
+                headerShown: true,
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </ApolloProvider>
   );
 }
 
---
+++ client/config/apolloClient.js
@@ -0,0 +1,40 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+// Andre;
+const client = new ApolloClient({
+  uri: "http://192.168.1.2:4000",
+  cache: new InMemoryCache(),
+});
+
+// Tondiki
+// const client = new ApolloClient({
+//   uri: "http://192.168.1.4:4000",
+//   cache: new InMemoryCache(),
+// });
+
+// Arie
+// const client = new ApolloClient({
+//   uri: "http://192.168.1.4:4000",
+//   cache: new InMemoryCache(),
+// });
+
+// Whisnu
+// const client = new ApolloClient({
+//   uri: "http://192.168.1.4:4000",
+//   cache: new InMemoryCache(),
+// });
+
+// Michael
+// const client = new ApolloClient({
+//   uri: "http://192.168.1.4:4000",
+//   cache: new InMemoryCache(),
+// });
+
+// const client = new ApolloClient({
+//   uri: "https://rome-service-orchestrator.herokuapp.com",
+//   cache: new InMemoryCache(),
+// });
+
+//
+
+export default client;
